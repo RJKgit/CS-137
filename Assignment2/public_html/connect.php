e57bc553356f7515bb22fce7c461f53c335858b6
Victor M Masivi <vm2@zinnex.com>
2016-05-02 03:39:58 -0700

    Update connect.php

--- Assignment2/public_html/connect.php
+++ Assignment2/public_html/connect.php
@@ -1,14 +1,12 @@
 <?php
-$servername = 'localhost';
-$username = 'root';
-$password = '';
+$servername = 'sylvester-mccoy-v3.ics.uci.edu';
+$username = 'inf124grp06';
+$password = 'secret';
 $dbname = "inf124grp06";
-
 // Create connection
 $conn = new mysqli($servername, $username,$password, $dbname);
-
 // Error for bad connection
 if ($conn->connect_error) {
-    die('Connection failed: '.$conn->connect_error);
+    die('Connection failed: '.$conn->connect_error);
 } 
 ?>
