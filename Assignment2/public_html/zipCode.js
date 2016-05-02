03f14a9ae213acd522297edeb4f25d25da4677ab
Victor M Masivi <vm2@zinnex.com>
2016-05-02 04:06:26 -0700

    Update zipCode.js
    Some new info from lecture

--- Assignment2/public_html/zipCode.js
+++ Assignment2/public_html/zipCode.js
@@ -3,33 +3,52 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//  Ajax JavaScript code for the order.html document
 
- var xhr;
- var response;
- var city;
- var state;
+/********************************************************/
+// function getPlace
+//  parameter: zip code
+//  action: create the XMLHttpRequest object, register the
+//          handler for onreadystatechange, prepare to send
+//          the request (with open), and send the request,
+//          along with the zip code, to the server
+//  includes: the anonymous handler for onreadystatechange, 
+//            which is the receiver function, which gets the 
+//            response text, splits it into city and state,
+//            and puts them in the document
 
- function getPlace(zip) { 
-        xhr = new XMLHttpRequest();
-        xhr.onreadystatechange =processXhrUpdate;
-        xhr.open("GET", "getCityState.php?zip=" + zip, true);
-        xhr.setRequestHeader("content-type","application/x-www-form-urlencoded");
-        xhr.send("zip=" + zip);    
- }
+  
+function getPlace (zip)
+{
+  if (window.XMLHttpRequest)
+  {  // IE7+, Firefox, Chrome, Opera, Safari
+     var xhr = new XMLHttpRequest();
+  }
+  else
+  {  // IE5, IE6
+     var xhr = new ActiveXObject ("Microsoft.XMLHTTP");
+  }
 
- function processXhrUpdate() {
-     if (xhr.readyState===4) {
-         if (xhr.status===200) {
- document.forms[0].output.value  = xhr.responseText;
- response  = xhr.responseXML.documentElement;
- city = response.getElementsByTagName('city')[0].firstChild.data;
- state = response.getElementsByTagName('state')[0].firstChild.data;
- document.forms[0].city.value = city;
- document.forms[0].state.value = state;
-         } else {
-             alert("Please enter a valid zip code:\n" +
-                 xhr.statusText);
-         }
-     }
- }
+  // Register the embedded handler function
+  // This function will be called when the server returns
+  // (the "callback" function)
+  xhr.onreadystatechange = function ()
+  { // 4 means finished, and 200 means okay.
+    if (xhr.readyState == 4 && xhr.status == 200)
+    { // Data should look like "Fairfax, Virginia"
+      var result = xhr.responseText;
+      var place = result.split (', ');
+      //if (document.getElementById ("city").value == "")
+        document.getElementById ("city").value = place[0];
+      //if (document.getElementById ("state").value == "")
+        document.getElementById ("state").value = place[1];
+    } 
+  }
+  // Call the response software component
+  xhr.open ("GET", "getCityState.php?zip=" + zip, true);
+  xhr.send ();
+  //xhr.open ("POST", "getCityState.php", true);
+  xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+  xhr.send ("zip="+zip);  
+}
 
